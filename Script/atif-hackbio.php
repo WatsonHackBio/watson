9ea070779ce63e6870d83cbce5d3de636d252dae
Sakshi Singh <92523465+WatsonHackBio@users.noreply.github.com>
2021-10-16 13:16:46 +0100

    Add files via upload

---
+++ Script/atif-hackbio.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+
+$Name = 'Muhammad Atif';
+$Email = 'itsatifiqbal@gmail.com';
+$SlackUsername = '@Atif';
+$Biostack = 'Genomics';
+
+
+echo "$Name,$Email,$SlackUsername,$Biostack";
+?>
