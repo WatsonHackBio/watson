ac55ee68adfef97eb6fdfa5130faed12dac6a770
Team Watson <92523465+WatsonHackBio@users.noreply.github.com>
2021-10-17 20:28:46 +0530

    Update watson.sh

--- watson.sh
+++ watson.sh
@@ -20,3 +20,23 @@ do
 	# which are then printed out to watson.csv
 	./$i | awk -F ',' '{ OFS="\t";print $1,$2,$3,$4,$5,$6}' >> watson.csv
 done
+
+
+#!/bin/bash
+echo "***Starting...***"
+echo "***Cloning repo...***"
+
+git clone https://github.com/thevalueadder/lovelace
+cd lovelace
+
+echo "***Mixing personal info...***"
+echo "Name,email,slack_username,biostack,twitter_handle,hamming_distance">>team_lovelace.csv
+node stage_0_JuanmaGarcia.js >>team_lovelace.csv
+node stage_0_ToheebAmoo.js >>team_lovelace.csv
+node stage_0_oluwadamife.js >>team_lovelace.csv
+node stage_0_SalmaElShafie.js >>team_lovelace.csv
+python stage_0_EstherOpone.py >>team_lovelace.csv
+python stage_0_SamAdeneye.py >>team_lovelace.csv
+python stage_0_ukaegbuDaniel.py >>team_lovelace.csv
+
+echo "***CSV generated!***"
